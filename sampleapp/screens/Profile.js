86cd1fd28f4638d9cd2e06891a64945b57dea5df
Bruno Barbosa <bruno.barbosa@keyrus.com.br>
2018-08-15 16:25:16 -0300

    Added profile photo

--- sampleapp/screens/Profile.js
+++ sampleapp/screens/Profile.js
@@ -1,13 +1,48 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 import { Card, Button, Text } from "react-native-elements";
 import { onSignOut } from "../auth";
 
 export default ({ navigation }) => (
   <View style={{ paddingVertical: 20 }}>
     <Card title="John Doe">
-      <View
-        style={{
+      
+    <View style={styles.imageContainer}>
+        <Image style={styles.profileImage} source={require('../images/woman.jpg')}/>  
+      </View>
+
+    <Button
+        backgroundColor="#03A9F4"
+        title="SIGN OUT"
+        onPress={() => onSignOut().then(() => navigation.navigate("SignedOut"))}
+      />
+            
+    </Card>
+  </View>
+);
+
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop:60
+  },
+  imageContainer: {
+    width: 80,
+          height: 80,
+    borderRadius: 64,
+      alignSelf: "center",
+          marginBottom: 20,
+      backgroundColor: "#bcbec1",
+          alignItems: "center",
+          justifyContent: "center"
+  },
+  profileImage: {
+    width: 80,
+          height: 80,
+    borderRadius: 64
+  },
+  defaultImageContainer: {
           backgroundColor: "#bcbec1",
           alignItems: "center",
           justifyContent: "center",
@@ -16,15 +51,5 @@ export default ({ navigation }) => (
           borderRadius: 40,
           alignSelf: "center",
           marginBottom: 20
-        }}
-      >
-        <Text style={{ color: "white", fontSize: 28 }}>JD</Text>
-      </View>
-      <Button
-        backgroundColor="#03A9F4"
-        title="SIGN OUT"
-        onPress={() => onSignOut().then(() => navigation.navigate("SignedOut"))}
-      />
-    </Card>
-  </View>
-);
+        }
+});
